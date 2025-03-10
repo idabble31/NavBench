fec56793c8b625315204c60e68b55593cccadbca
idabble31 <rafly.daffaldi@gmail.com>
2025-03-10 22:55:21 +0700

    chore: remove buld and devel folders

--- build/aws-robomaker-small-warehouse-world/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raflydaffaldi/lrf_1/src/aws-robomaker-small-warehouse-world
-# Build directory: /home/raflydaffaldi/lrf_1/build/aws-robomaker-small-warehouse-world
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/mobile_manipulator_body/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raflydaffaldi/lrf_1/src/mobile_manipulator_body
-# Build directory: /home/raflydaffaldi/lrf_1/build/mobile_manipulator_body
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
